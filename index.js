77624d8d648b53c4bb71c3725e316c5bdd65bae4
zaidkhan2625 <zaidkhan262523@gmail.com>
2024-05-09 19:25:46 +0530

    home page part done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+app.get('/',(req , res)=>{
+    res.send("hello i am live ");
+})
+app.listen(2000 , ()=>{
+console.log("app is listinig");
+})
+
