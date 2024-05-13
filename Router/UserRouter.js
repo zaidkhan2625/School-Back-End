7571470bb10100011016566426bc13b2d9c8fb28
zaidkhan2625 <zaidkhan262523@gmail.com>
2024-05-13 23:14:09 +0530

    allmost complted

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 // const bcrypt = require("bcrypt"); // Import bcrypt for password hashing
+const bcrypt = require("bcryptjs");
 const {newAdmin} = require("../DatabseFolder/DatabseforUser");
 route.get('/user', (req, res) => {
     res.send("hello user i am here");
@@ -13,7 +14,7 @@ route.post('/addAdmin', async (req, res) => {
         if (userExists) {
             res.send("Email already exists");
         } else if (name && Email && password) {
-            // const hashedPassword = await bcrypt.hash(password, 10);
+            const hashedPassword = await bcrypt.hash(password, 10);
             await newAdmin.create({ name, Email, password: password, role: 'admin' }); // Assuming 'role' is defined
             res.send("Admin added successfully");
         } else {
