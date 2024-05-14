2e25be92aee7a230b9f075a4190d41ef17fc362d
zaidkhan2625 <zaidkhan262523@gmail.com>
2024-05-14 14:08:58 +0530

    allmost complted

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -1,43 +1,70 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const {newAdmin} = require("../DatabseFolder/DatabseforUser");
-const addAdmin  = async (req, res) => {
-    const { name, Email, password } = req.body;
-    try {
-        const userExists = await newAdmin.findOne({ Email }); // Assuming newUser is a Mongoose model
-        if (userExists) {
-            res.send("Email already exists");
-        } else if (name && Email && password) {
-            const hashedPassword = await bcrypt.hash(password, 10);
-            await newAdmin.create({ name, Email, password: password, role: 'admin' }); // Assuming 'role' is defined
-            res.send("Admin added successfully");
-        } else {
-            res.send("Incomplete data provided");
-        }
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.status(500).send("Internal Server Error");
+const { newAdmin } = require("../DatabseFolder/DatabseforUser");
+const addAdmin = async (req, res) => {
+  const { name, Email, password } = req.body;
+  try {
+    const userExists = await newAdmin.findOne({ Email }); // Assuming newUser is a Mongoose model
+    if (userExists) {
+      res.send("Email already exists");
+    } else if (name && Email && password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await newAdmin.create({ name, Email, password: password, role: "admin" }); // Assuming 'role' is defined
+      res.send("Admin added successfully");
+    } else {
+      res.send("Incomplete data provided");
     }
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
 };
 const loginAdmin = async (req, res) => {
-    const { Email, password } = req.body;
-    try {
-        const user = await newAdmin.findOne({ Email }); 
-        if (!user) {
-            return res.status(404).send("User not found"); 
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).send("Invalid password"); 
-        }
-        const token = jwt.sign({ userId: user._id, email: user.Email }, 'your_secret_key', { expiresIn: '1h' });
-        res.cookie('token', token, { httpOnly: true });
-        res.json({status:"Login successful",token:token,data:user});
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.status(500).send("Internal Server Error");
+  const { Email, password } = req.body;
+  try {
+    const user = await newAdmin.findOne({ Email });
+    if (!user) {
+      return res.status(404).send("User not found");
     }
-};
 
-module.exports = {addAdmin, loginAdmin};
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).send("Invalid password");
+    }
+    const token = jwt.sign(
+      { userId: user._id, email: user.Email },
+      "your_secret_key",
+      { expiresIn: "1h" }
+    );
+    res.cookie("token", token, { httpOnly: true });
+    res.json({ status: "Login successful", token: token, data: user });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
+const authenticateAdmin = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, "your_secret_key");
+
+    if (!decoded) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized - Admin role required" });
+    }
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Error:", error.message);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+const userAdd = async(req , res)=>{
+    res.send("hello");
+}
+module.exports = { addAdmin, loginAdmin, authenticateAdmin,userAdd };
--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const route = express.Router();
-const { addAdmin, loginAdmin } = require("../Controller/usercontroller");
+const { addAdmin, loginAdmin, authenticateAdmin, userAdd } = require("../Controller/usercontroller");
 route.get('/user', (req, res) => {
     res.send("hello user i am here");
 });
 
 route.post('/addAdmin', addAdmin);
 route.get('/loginAdmin' , loginAdmin);
+route.post('/useradd' , authenticateAdmin,userAdd);
 
 module.exports = route;
